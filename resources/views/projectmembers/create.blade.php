7d4da614a0af6de4ea066eb901835bff4f0baf91
Nachomatt <matthijsdell1@outlook.com>
2020-02-13 17:11:05 +0100

    Added fancy animation to the project members create

--- resources/views/projectmembers/create.blade.php
+++ resources/views/projectmembers/create.blade.php
@@ -4,13 +4,28 @@
 
 
 @section('content')
-    <a class="goback" href="{{route('projects.show',$project->id)}}">Go Back</a>
-    <form action="{{route('projects.projectmembers.store',$project->id)}}" method="post">
-        @csrf
-         @foreach($users as $user)
-            <span class="knoptekst permissionlijst">{{$user->name}}: <input type="checkbox" name="users[]" value="{{$user->id}}"></span><br>
-            @endforeach
-        <br> <button type="submit">Submit</button>
-    </form>
+    <div class="container text-white">
+        <form action="{{route('projects.projectmembers.store',$project->id)}}" method="post">
+            @csrf
+            <h2 class=" text-white text-center">All Users</h2>
+            <div class="card bg-dark border-top border-left border-bottom">
+                <div class="row mr-0">
+                    @foreach($users as $user)
+                        <div class="col-3 border-right wow fadeInLeft" data-wow-delay="0.{{$user->id}}s">
+                            <span>{{$user->name}}: <input type="checkbox" name="users[]"
+                                                          value="{{$user->id}}"></span><br>
+                        </div>
+
+                    @endforeach
+                </div>
+            </div>
+
+            <br>
+            <div class="d-flex justify-content-center">
+                <a class="btn btn-primary goback mr-2" href="{{route('projects.show',$project->id)}}">Go Back</a>
+                <button class="btn btn-success" type="submit">Submit</button>
+            </div>
+        </form>
+    </div>
 @stop
 @endcan
