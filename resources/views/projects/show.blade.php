69b617eb7b69c647002827c95311a0f2fe050271
Nachomatt <matthijsdell1@outlook.com>
2020-02-13 15:40:50 +0100

    Made show project code look more neato

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -6,8 +6,12 @@
                 <ul class="nav">
                     <li>
                         <h5 class="text-dark">Project Name: {{$project->name}}</h5><br>
-                        <h5 class="text-dark">Project User: <a href="{{route('projects.projectmembers.show',[$project->id,Auth::user()->id])}}">{{Auth::user()->name}}</a></h5>
-                        <h5 class="text-dark"><a href="{{route('projects.projectmembers.show',[$project->id,Auth::user()->id])}}">Show Profile</a></h5>
+                        <h5 class="text-dark">Project User: <a
+                                href="{{route('projects.projectmembers.show',[$project->id,Auth::user()->id])}}">{{Auth::user()->name}}</a>
+                        </h5>
+                        <h5 class="text-dark"><a
+                                href="{{route('projects.projectmembers.show',[$project->id,Auth::user()->id])}}">Show
+                                Profile</a></h5>
 
                     </li>
                 </ul>
@@ -25,20 +29,21 @@
                 @endforeach
             </div>
             <div class="bg-dark">
-            <a class="btn btn-primary w-25 mb-1" href="{{route('projects.index')}}">Go Back</a>
-            @can('Moderate Website')
-                    <span><a class="btn btn-success mb-1" href="{{route('projects.projectmembers.create',$project->id)}}">Add Project Members</a>
+                <a class="btn btn-primary w-25 mb-1" href="{{route('projects.index')}}">Go Back</a>
+                @can('Moderate Website')
+                    <span><a class="btn btn-success mb-1"
+                             href="{{route('projects.projectmembers.create',$project->id)}}">Add Project Members</a>
                 <a class="btn btn-primary mb-1" href="{{route('projects.edit',$project->id)}}">Edit project</a>
                     </span>
                 @endcan
-            <form class="d-flex justify-content-center" action="{{ route('projects.destroy', $project->id) }}"
-                  method="post">
-                @csrf @method('delete')
-                <button type="submit" class="btn btn-danger d-flex justify-content-center"
-                        onclick="confirm('Are you sure, you want to delete project: {{ $project->name }}?');">Delete
-                    Project
-                </button>
-            </form>
+                <form class="d-flex justify-content-center" action="{{ route('projects.destroy', $project->id) }}"
+                      method="post">
+                    @csrf @method('delete')
+                    <button type="submit" class="btn btn-danger d-flex justify-content-center"
+                            onclick="confirm('Are you sure, you want to delete project: {{ $project->name }}?');">Delete
+                        Project
+                    </button>
+                </form>
             </div>
     </div>
     @endcan
